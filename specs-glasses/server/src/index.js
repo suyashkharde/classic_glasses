c73d947516f03e249d4d6543783314e22b9a6baf
suyashkharde <khardesuyash00@gmail.com>
2026-04-16 00:29:31 +0530

    deployed

--- specs-glasses/server/src/index.js
+++ specs-glasses/server/src/index.js
@@ -38,7 +38,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT || 5000, () =>
+    app.listen(process.env.PORT || 5000, '0.0.0.0', () =>
       console.log(`Server running on port ${process.env.PORT || 5000}`)
     );
   })
